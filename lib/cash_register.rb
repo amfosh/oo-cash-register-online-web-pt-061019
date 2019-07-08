57e6067c1a56f2f44caeac13311b78e2751c5d04
Alicia Fasciocco <afasciocco@gmail.com>
2019-07-08 11:22:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,7 @@ class CashRegister
   def apply_discount(total, discount)
     if @discount > 0 
       @discount = @discount/100.to_f 
-      @total = @total - (@discount * 100)
+      @total = @total - (total * 100)
   end
 end
 
