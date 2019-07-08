6b680854ecf17607000569ad0a2c46bb011d40a0
Alicia Fasciocco <afasciocco@gmail.com>
2019-07-08 10:54:03 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,7 +3,7 @@ class CashRegister
   
   def initialize(total = 0)
     @total = total
-    @cash_register_with_discount = 20
+    @cash_register_with_discount = cash_register_with_discount
   end
 end
     
