557ea256ecda78d9b2a3f3c0a696a1020a2c9d2e
Alicia Fasciocco <afasciocco@gmail.com>
2019-07-08 11:01:37 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,8 +1,9 @@
 class CashRegister
   attr_accessor :total 
   
-  def initialize(total = 0)
+  def initialize(total = 0, discount = 0)
     @total = total
+    @discount = discount
   end
 end
     
